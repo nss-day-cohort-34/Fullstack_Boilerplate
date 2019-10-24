0b2a356754584f3212d5f6373786f9cd17a43ff6
Adam Sheaffer <adam.e.sheaffer@gmail.com>
2019-10-23 20:53:36 -0500

    example fetch call

--- Nutshell/client/src/components/Home.js
+++ Nutshell/client/src/components/Home.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { createAuthHeaders } from '../API/userManager';
 
-function Home(props) {
-  return (
-    <h1>Welcome to my app</h1>
-  )
+class Home extends Component {
+  state = {
+    values: [],
+  }
+
+  componentDidMount() {
+    const authHeader = createAuthHeaders();
+    fetch('/api/v1/values', {
+      headers: authHeader
+    })
+      .then(response => response.json())
+      .then(values => {
+        this.setState({ values: values });
+      });
+  }
+
+  render() {
+    return (
+      <>
+        <h1>Welcome to my app</h1>
+        <ul>
+          {
+            this.state.values.map(value => <li>{value}</li>)
+          }
+        </ul>
+      </>
+    )
+  }
 }
 
 export default Home;
