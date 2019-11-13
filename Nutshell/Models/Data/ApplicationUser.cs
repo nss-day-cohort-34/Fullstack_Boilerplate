cb9e3c5c25b63844bbf5e5f5c4cfe22b7512ef26
Adam Sheaffer <adam.e.sheaffer@gmail.com>
2019-11-12 19:26:06 -0600

    add user view model and warning comment in data model

--- Nutshell/Models/Data/ApplicationUser.cs
+++ Nutshell/Models/Data/ApplicationUser.cs
@@ -6,6 +6,10 @@ using System.Threading.Tasks;
 
 namespace Capstone.Models.Data
 {
+    // ******************* 
+    // NOTE: Do NOT return an ApplicationUser as part of a json response.
+    // It includes sensitive information on it.
+    // Instead, always convert an ApplicationUser to an ApplicationUserViewModel before it get sent as JSON
     public class ApplicationUser : IdentityUser
     {
         public string FirstName { get; set; }
---
+++ Nutshell/Models/ViewModels/ApplicationUserViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Capstone.Models.ViewModels
+{
+    public class ApplicationUserViewModel
+    {
+        public string Id { get; set; }
+        public string Username { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
